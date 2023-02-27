a5fbbc5fd8b0ea6cbf60db3371ce27392fd61d05
ismael strey pereira <ismaelstrey@hotmail.com>
2023-02-27 08:02:44 -0300

    dd

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { routes } from "./routes";
-
+const PORT = 4000
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log("Server rodadno em http://localhost:3333"));
+app.listen(PORT, () => console.log("Server rodadno em http://localhost:4000"));
